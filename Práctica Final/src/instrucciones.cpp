e66d2ee3de97774e5fadc1a3dfa56071d4dc1b2b
pacoapm <50922242+pacoapm@users.noreply.github.com>
2019-12-26 19:02:00 +0100

    Terminado operador entrada instrucciones, hay que testearlo

--- Práctica Final/src/instrucciones.cpp
+++ Práctica Final/src/instrucciones.cpp
@@ -39,7 +39,6 @@ istream& operator >>(istream& in, instrucciones &instrucciones){
     
     while(linea.size() != 0){
         
-        busca_accion = true;
         for(int i = 0; i < linea.size() && busca_accion; i++){
             //caso en el que la accion y los ingredientes esten en la misma linea, ejemplo: cortar lechuga
             if(linea[i] == ' '){
@@ -47,7 +46,7 @@ istream& operator >>(istream& in, instrucciones &instrucciones){
                 if(busca_accion){
                     subcadena = linea.substr(comienzo,fin-comienzo); //nombre de la accion
                     ariedad = instrucciones.ariedad(subcadena); //ariedad de la accion
-                    ArbolBinario<string> arbol(subcadena); //creamos el arbol con el nombre de la accion como etiqueta
+                    //ArbolBinario<string> arbol(subcadena); //creamos el arbol con el nombre de la accion como etiqueta
                     comienzo = fin;
                     comienzo++;
                     busca_accion = false;
@@ -59,18 +58,24 @@ istream& operator >>(istream& in, instrucciones &instrucciones){
         
         //casos en el que aparece una accion y un ingrediente en al misma linea
         if(subcadena.size() > 1){
-            
+            ArbolBinario<string> arbol(subcadena);//creamos el arbol con la accion como raiz
             if(ariedad == 1){ //caso en el que la accion sea unaria y el ingrediente este en la misma linea
-            subcadena = linea.substr(comienzo,subcadena.size() - comienzo);
-            //insertar hijo a la izquierda
-            //meter en la pila
+                subcadena = linea.substr(comienzo,subcadena.size() - comienzo);
+                //insertar hijo a la izquierda
+                arbol.Insertar_Hi(arbol.getRaiz(),subcadena);
+                //meter en la pila
+                p.push(arbol);
             }
         
             if(ariedad == 2){ //caso en el que la accion sea binaria y aparezca un elemento en la misma linea linea
                 subcadena = linea.substr(comienzo,subcadena.size() - comienzo);
                 //insertar como hijo a la derecha
+                arbol.Insertar_Hd(arbol.getRaiz(),subcadena);
                 //y coger elemeto de la pila e insertarlo como hijo a la izquierda
+                arbol.Insertar_Hi(arbol.getRaiz(),p.top());
+                p.pop();
                 //finalmente meterlos en la pila
+                p.push(arbol);
             }
             
         }else{ //en este caso aparece solo la accion en la linea
@@ -80,10 +85,18 @@ istream& operator >>(istream& in, instrucciones &instrucciones){
             
             if(ariedad == 1){
              //meter elemento que haya en la pila como hijo a la izquierda
+                arbol.Insertar_Hi(arbol.getRaiz(),p.top());
              //meterlo en la pila
+                p.pop();
+                p.push(arbol);
             }else{ //si la ariedad == 2
                 //insertar primero hijo a la derecha y luego hijo a la izquierda
+                arbol.Insertar_Hd(arbol.getRaiz(),p.top());
+                p.pop();
+                arbol.Insertar_Hd(arbol.getRaiz(), p.top());
+                p.pop();
                 //meterlo en la pila
+                p.push(arbol);
             }
             
         }
